e5c342b5442bb17c3ab1c8e3523427b95893194b
Joey Bonventre <Jbonventre95@gmail.com>
2022-02-11 13:21:15 -0500

    Added matching password validation

--- scripts.js
+++ scripts.js
@@ -1,18 +1,12 @@
 
 
-let form = document.querySelector('form');
-let password = form.getElementById('password').value;
-let passwordConfirmation = form.getElementById('password-confirm').value;
-let createAccountButton = form.getElementById('createAccount');
-let error = form.getElementById('error');
-
-
-createAccountButton.addEventListener('click', (e) => {
-
-    if(password != passwordConfirmation) {
-        e.preventDefault();
-        error.removeAttribute('hidden');
-
-    }
-    error.removeAttribute('hidden');
-});
+let button = document.querySelector('.createAccount').onclick = function(event) {
+     let password = document.querySelector('#password').value;
+     let confirmPassword = document.querySelector('#confirm-password').value;
+     
+     if (password != confirmPassword) {
+        document.querySelector('.error').removeAttribute('hidden');
+         event.preventDefault();
+        }
+        
+}
